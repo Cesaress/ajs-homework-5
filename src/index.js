d5783a1f858e173672853664e6830ae8e3070026
Андрей Богданов <cesaress@yandex.ru>
2023-03-26 20:53:03 +0300

    hw 5.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 
+import {
+    Swordsman, Bowman, Magician, Daemon, Undead, Zombie,
+  } from './js/basic';
+  
+  const persons = [];
+  const persDaemon = new Daemon('Larry');
+  const persSwordsman = new Swordsman('Chack');
+  const persBowman = new Bowman('Susanna');
+  const persUndead = new Undead('Shon');
+  const persMagician = new Magician('Din');
+  const persZombie = new Zombie('Will');
+  persons.push(persBowman, persDaemon, persMagician, persUndead, persSwordsman, persZombie);
+  export default persons;
+  // console.log(persons);
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,44 +1,24 @@
 // eslint-disable-next-line max-classes-per-file
 class Character {
-    health = 100;
+  health = 100;
 
-    level = 1;
+  level = 1;
 
-    attack;
+  attack;
 
-    defence;
+  defence;
 
-    constructor(name, type) {
-      this.name = name;
-      this.type = type;
-      if (typeof this.name !== 'string' || this.name.length > 10 || this.name.length < 2) {
-        throw new Error('Имя персонажа введено неверно');
-      }
-      if (!['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(this.type)) {
-        throw new Error('Тип персонажа введен неверно');
-      }
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+    if (typeof this.name !== 'string' || this.name.length > 10 || this.name.length < 2) {
+      throw new Error('Имя персонажа введено неверно');
     }
-
-    levelUp() {
-      if (this.health > 0) {
-        this.level += 1;
-        this.attack *= 1.2;
-        this.defence *= 1.2;
-        this.health = 100;
-        return this;
-      }
-      throw new Error('Sorry, ничего не полчится, Вы мертвы');
+    if (!['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(this.type)) {
+      throw new Error('Тип персонажа введен неверно');
     }
-
-    demage(points) {
-      if (this.health > 0) {
-        this.health -= points * (1 - this.defence / 100);
-        return this;
-      }
-      throw new Error('Sorry, ничего не полчится, Вы мертвы');
-    }
+  }
 }
-
 export default Character;
 export class Bowman extends Character {
   constructor(name) {
@@ -87,7 +67,3 @@ export class Daemon extends Character {
     this.defence = 40;
   }
 }
-
-const pers = new Daemon('Larry');
-pers.levelUp();
-console.log(pers);
